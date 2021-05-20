1f39fb602d3ddef2b971f475c1d29566a2c5d063
Fagner Santos <fagner.0001@outlook.com>
2021-05-20 18:38:29 -0300

    primeiro commit

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './Header.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <nav className={`${style.nav} container`}>
+        <Link className={style.logo} to="/" aria-label="Dogs - Home">
+          {' '}
+          <Dogs />{' '}
+        </Link>
+        <Link className={style.login} to="/login">
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={style.home}>
+      <p>home</p>
+    </div>
+  );
+};
+
+export default Home;
