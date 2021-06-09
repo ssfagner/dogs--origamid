71dbc709dafa7c7f0814dcaf16b2f198c2e6903f
Fagner Santos <fagner.0001@outlook.com>
2021-06-09 20:10:46 -0300

    UserHeader

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 import { UserContext } from '../UserContext';
 
 const Header = () => {
-  const { data, userLogout } = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return (
     <header className={style.header}>
@@ -17,7 +17,6 @@ const Header = () => {
         {data ? (
           <Link className={style.login} to="/conta">
             {data.nome}
-            <button onClick={userLogout}>Sair</button>
           </Link>
         ) : (
           <Link className={style.login} to="/login">
