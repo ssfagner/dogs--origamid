b2bdb3e3550715d1351f3338757b0ba9b8c69745
Fagner Santos <fagner.0001@outlook.com>
2021-06-22 18:35:01 -0300

    Estilo comentário

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { UserContext } from '../../UserContext';
 import PhotoCommentsForm from './PhotoCommentsForm';
-import styles from './PhotoComments.module.css'
+import styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
-
   const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null);
+  const { login } = React.useContext(UserContext);
 
-  const {login} = React.useContext(UserContext)
+  React.useEffect(() => {
+    commentsSection.current.scroolTop = commentsSection.current.scroolHeight;
+  }, [comments]);
 
   return (
-  <>
-    <ul className={styles.comments}>
-      {comments.map(comment => <li key={comment.comment_ID}>
-        <b>{comment.comment_author}: </b>
-        <span>{comment.comment_content}</span>
-      </li>)}
-    </ul>
-    {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
-  </>
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
   );
 };
 
